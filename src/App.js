4571548ede7859da170eb16c258a8a00b91ba5d4
Rafael Muela <rafael316muela@gmail.com>
2022-01-14 20:56:28 +0100

    Emulacion de llamada a API externa

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Loading  from './components/Loading';
-
+import List from './components/List';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <Loading/>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+        <List/>
+       
       </header>
     </div>
   );
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-
+import propTypes from 'prop-types';
 class Loading extends Component {
     render() {
-        return (<div className="loading"> Cargando ....</div>);
+        return (<div className="loader">{this.props.message || 'Cargando ...'}</div>);
     }
 }
 
+Loading.propTypes = {
+    message: propTypes.string.isRequired
+};
 export default Loading;
